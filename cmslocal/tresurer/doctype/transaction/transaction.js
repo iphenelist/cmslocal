d6e1b7a9db6a6b563647c372056855f5fbf98e80
iphenelist <innocentphenelist@gmail.com>
2024-07-24 00:06:36 +0300

    fix: adding naming series and remove transaction doctype

--- cmslocal/tresurer/doctype/transaction/transaction.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, Innocent PM and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Transaction", {
-// 	refresh(frm) {
-
-// 	},
-// });
